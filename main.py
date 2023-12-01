0f2375456341c6dadefd8d95583f5f67692c10ef
Alibehzad79 <Alibehzad13791122@gmail.com>
2023-12-01 13:58:08 +0330

    fix Xpath bugs and update to v1.2.0

--- main.py
+++ main.py
@@ -274,7 +274,6 @@ def main(page: ft.Page):
     password = ft.TextField(label="password", password=True, can_reveal_password=True)
     message = ft.TextField(
         label="Message",
-        value="سلام وقت بخیر برای افزایش فالور و لایک و ... به پیچ زیر پیام بفرستید @flowere_buy"
     )
     submit_btn = ft.ElevatedButton(
         text="Submit",
